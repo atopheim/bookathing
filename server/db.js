ed271500a835146ecae4a3af9997a049e9a9dcd7
atopheim <torbjornop@gmail.com>
2022-09-14 11:29:52 +0200

    test

--- server/db.js
+++ server/db.js
@@ -1,19 +1,21 @@
 const admin = require("firebase-admin");
 
-serviceAccountPath = "./serviceAccountKey.json"
-try {
-  if (fs.existsSync(serviceAccountPath)) {
-    const serviceAccount = require(serviceAccountPath);
-  }
-} catch(err) {
-    const serviceAccount = process.env.FIREBASE_ACCOUNT_KEY;
-    if (!serviceAccount){
-      throw 'No service account credentials were provided';
-    }
-}
+// serviceAccountPath = "./serviceAccountKey.json"
+// try {
+//   if (fs.existsSync(serviceAccountPath)) {
+//     const serviceAccount = require(serviceAccountPath);
+//   }
+// } catch(err) {
+//     const serviceAccount = process.env.FIREBASE_ACCOUNT_KEY;
+//     console.log("->>",serviceAccount)
+//     console.log("->>",process.env.DB_HOST)
+//     if (!serviceAccount){
+//       throw 'No service account credentials were provided';
+//     }
+// }
 
 module.exports = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(process.env.FIREBASE_ACCOUNT_KEY),
 });
 
 const db = admin.firestore();
